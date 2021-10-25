95cb2ac7df511241b9af3c0274b05b10f08e4c66
unknown <harisprasetya46@gmail.com>
2021-10-25 23:33:10 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+    home: BmiCalculator(),
+  ),
+);
+
+  class BmiCalculator  extends StatelessWidget {
+    const BmiCalculator ({Key? key}) : super(key: key);
+  
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Container(
+          child: Row( 
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('Halo Selamat Belajar Flutter'),
+              Image.network('https://picsum.photos/250?image=9'),
+              Image.asset('assets/img/praktikum.jpeg')
+            ],
+          ),
+        ),
+      );
+    }
+  }
